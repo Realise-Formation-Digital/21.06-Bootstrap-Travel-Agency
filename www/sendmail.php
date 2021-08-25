b9f704ad5d024fd329c69b2c28e62c3982d8d70b
IEDIN2021 <85932847+IEDIN2021@users.noreply.github.com>
2021-08-25 13:14:39 +0200

    update contact.php et sendmail.php

--- www/sendmail.php
+++ www/sendmail.php
@@ -1,4 +1,6 @@
 <?php
+
+
 if(isset($_POST['submit'])){
 
     //collection de donnés
@@ -7,9 +9,19 @@ if(isset($_POST['submit'])){
     $tel = $_POST['tel'];
     $message = $_POST['message'];
 
-        # Title of the CSV
+    if (empty($name) || empty($email) || empty($tel) || empty($message)) {
         
-        $Content = "Nom, E-mail, Téléphone, Message\n";
+        $error = 'Merci de remplire tous les champs avec *';
+        //return true;
+        } else {
+
+            $confirmation = " " . $name . ", merci pour votre message, nous vous répondrons dans les plus brefs délais";
+        }
+
+
+    
+        # Title of the CSV
+        //$Content = "Nom, E-mail, Téléphone, Message\n";
 
         //set the data of the CSV
         $Content .= "$name; $email; $tel; $message\n";
@@ -23,10 +35,8 @@ if(isset($_POST['submit'])){
         file_put_contents($FileName, $Content, FILE_APPEND | LOCK_EX);
 
 
-        mail($name,$email,$tel,$message); // sends a copy of the message to the sender
-        echo " " . $name . ", merci pour votre message, nous vous répondrons dans les plus brefs délais";
-        // You can also use header('Location: thank_you.php'); to redirect to another page
-
+        //mail($name,$email,$tel,$message); 
+       // $confirmation = " " . $name . ", merci pour votre message, nous vous répondrons dans les plus brefs délais";
 
         
 
@@ -36,16 +46,5 @@ if(isset($_POST['submit'])){
         //exit();
     
 }
+
 //ako imas greske
-
-
-
-?> 
-<!--
-<form action='' method='post'>
-<p><label>Name</label><br><input type='text' name='name' value=''></p> 
-<p><label>Email</label><br><input type='text' name='email' value=''></p> 
-<p><input type='submit' name='submit' value='Submit'></p> 
-</form>
-
-!-->
