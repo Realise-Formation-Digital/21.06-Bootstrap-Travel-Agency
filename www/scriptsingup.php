b9ba50feb66bf9635d9734b5849758b286faa5bd
Dos Santos Davide <85991452+Superwinny@users.noreply.github.com>
2021-08-26 12:26:34 +0200

    last comit singup

--- www/scriptsingup.php
+++ www/scriptsingup.php
@@ -11,7 +11,7 @@
      $hashedpassword = md5($password);
      $hashedrpassword = md5($rpassword);
 
-     if (empty($nom) || empty($mail) || empty($hashedpassword) || empty($hashedrpassword)) {
+     if (empty($nom) || empty($mail) || empty($password) || empty($rpassword)) {
         
       $error = 'Merci de remplire tous les champs';
       //return true;
