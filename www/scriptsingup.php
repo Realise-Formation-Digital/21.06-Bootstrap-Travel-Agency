6356f30e4758ea4b58ab50a4906b659749cf6801
Dos Santos Davide <85991452+Superwinny@users.noreply.github.com>
2021-08-26 10:08:19 +0200

    comit sing up page update

--- www/scriptsingup.php
+++ www/scriptsingup.php
@@ -1,16 +1,16 @@
 <?php
    // Vérifier si le formulaire est soumis 
-   if ( isset( $_GET['submit'] ) ) {
+   if ( isset( $_POST['nom'] ) && isset( $_POST['mail'] ) && isset( $_POST['password'] ) && isset( $_POST['rpassword'] ) ) {
      /* récupérer les données du formulaire en utilisant 
         la valeur des attributs name comme clé 
        */
-     $nom = $_GET['nom']; 
-     $mail = $_GET['mail']; 
-     $password = $_GET['password'];
-     $rpassword = $_GET['rpassword'];
+     $nom = $_POST['nom']; 
+     $mail = $_POST['mail']; 
+     $password = $_POST['password'];
+     $rpassword = $_POST['rpassword'];
      $hashedpassword = md5($password);
      // afficher le résultat
-     echo '<h3>Informations récupérées en utilisant GET</h3>'; 
+     echo '<h3>Informations récupérées en utilisant POST</h3>'; 
      echo 'Nom : ' . $nom . ' Mail : ' . $mail . ' Password : ' . md5($password) . ' Rpassword : ' . md5($rpassword); 
        $Content .= "$nom; $mail; $hashedpassword; $rpassword\n";
        $FileName = "users.csv";
